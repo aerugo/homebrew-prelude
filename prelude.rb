208613158e3a8e4cd23cba3aef1b33d11f51c5ba
aerugo <hugiasgeirsson@gmail.com>
2024-07-23 20:31:42 +0200

    Fixed some style guide issues

--- prelude.rb
+++ prelude.rb
@@ -1,7 +1,7 @@
 class Prelude < Formula
-  desc "A tool to generate prompts containing file tree and concatenated file contents"
+  desc "Tool to generate prompts containing file tree and concatenated file contents"
   homepage "https://github.com/aerugo/prelude"
-  url "https://github.com/aerugo/prelude/archive/tags/v0.3.tar.gz"
+  url "https://github.com/aerugo/prelude/archive/refs/tags/v0.3.tar.gz"
   sha256 "ba1cc4ec19375707603da30ce5dec516e6171593f649b7d26fe327cb608c0f20"
   license "MIT"
 
