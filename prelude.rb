775401ccd758842ec56fdae369a4d39fb05b2635
Hugi <hugiasgeirsson@gmail.com>
2024-09-21 16:57:05 +0200

    Update prelude.rb

--- prelude.rb
+++ prelude.rb
@@ -1,8 +1,8 @@
 class Prelude < Formula
   desc "Tool to generate prompts containing file tree and concatenated file contents"
   homepage "https://github.com/aerugo/prelude"
-  url "https://github.com/aerugo/prelude/archive/refs/tags/v0.3.tar.gz"
-  sha256 "53329ba63061a2019f7f358f61c2fb7c368bb01e1f882fc5d5a044d5666288ab"
+  url "https://github.com/aerugo/prelude/archive/refs/tags/v0.31.tar.gz"
+  sha256 "45639c37c172cf68e04c7f13cf24b1ab29e276b48fac9655a897cc971774f61e"
   license "MIT"
 
   depends_on "tree"
